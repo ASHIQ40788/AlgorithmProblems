33b100b61906a48f357d36369e8478a52f2d1e24
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-10-03 14:10:53 +0530

    [ASHIQ] added : program file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms058
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //created an object with class name InsertionSort.
+            //InsertionSort insertion = new InsertionSort();
+            //insertion.InsertionSortOrder();
+            //Console.ReadLine();
+
+            //Created an object with class name BubbleSortProgram.
+            //BubbleSortProgram Bubble = new BubbleSortProgram();
+            //Bubble.BubbleSort();
+
+            BinarySearchWord BinaryWord = new BinarySearchWord();
+            BinaryWord.UserInput();
+            Console.ReadLine();
+        }
+    }
+}
+
