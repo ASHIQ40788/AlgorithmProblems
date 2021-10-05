22764bbb51dcc8c39f68b3243c4bcbb62857fe3d
ASHIQ40788 <86375129+ASHIQ40788@users.noreply.github.com>
2021-10-05 10:54:05 +0530

    Update BinarySearchWord.cs

--- BinarySearchWord.cs
+++ BinarySearchWord.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +6,40 @@ using System.Threading.Tasks;
 
 namespace AlgorithmPrograms058
 {
+
+    //created a class BinarySearchWord with Access Modifier public.
     public class BinarySearchWord
     {
         //Method for taking user input as word to find in the list
         public void UserInput()
         {
             string input;
+            //Asking the user to enter the colour for searching.
             Console.WriteLine("Enter the colour for searching in the arraylist:");
             input = Console.ReadLine();
             BinarySearch(input);
         }
+
+        //input is my parameter passing.
         public void BinarySearch(string input)
         {
             //colours in the array list.
-            string[]  colour_list = { "skyblue", "green", "red", "yellow", "orange", "violet" ,"white","black,","purple","blue"};
+            string[]  colour_list = { "skyblue", "green", "red", "yellow", "orange", "violet" ,"white","black,","purple"};
+
+            //Sorts the elements inn the entire array.
             Array.Sort(colour_list);
+            //bool gives you two statements : true or false
             bool colour_found = false;
             int start = 0;
             int end = colour_list.Length - 1;
             if (colour_list.Length >= 1)
             {
+
+                //finds the middle value of an array after sort.
                 int middle = start + end / 2;
+
+                //if result(-1) is less than 0 ,then else block executes.
+                //if result(1) is greater than 0 ,then if block executes.
                 int result = input.ToLower().CompareTo(colour_list[middle]);
                 if (result > 0)
                 {
@@ -44,9 +57,8 @@ namespace AlgorithmPrograms058
             }
             if (colour_found != true)
             {
-                Console.WriteLine("{0} colour is not present in colour_list", input);
-                
-                
+                 Console.WriteLine("{0} colour is not present in colour_list", input);
+               
             }
             else
             {
